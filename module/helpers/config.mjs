95f51386a5703dd925cd69b392a624f9aeaddb20
Draviand <draciandelta@gmail.com>
2023-10-04 15:47:21 -0400

    initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const GNURPS = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ GNURPS.abilities = {
+  "str": "GNURPS.AbilityStr",
+  "dex": "GNURPS.AbilityDex",
+  "con": "GNURPS.AbilityCon",
+  "int": "GNURPS.AbilityInt",
+  "wis": "GNURPS.AbilityWis",
+  "cha": "GNURPS.AbilityCha"
+};
+
+GNURPS.abilityAbbreviations = {
+  "str": "GNURPS.AbilityStrAbbr",
+  "dex": "GNURPS.AbilityDexAbbr",
+  "con": "GNURPS.AbilityConAbbr",
+  "int": "GNURPS.AbilityIntAbbr",
+  "wis": "GNURPS.AbilityWisAbbr",
+  "cha": "GNURPS.AbilityChaAbbr"
+};
